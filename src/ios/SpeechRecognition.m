ae19ce9c3d14e7df6db2a98f67521d5418ee5541
Veniamin Craciun <veniamin.craciun@tecnico.ulisboa.pt>
2018-01-13 00:55:22 +0000

    Update SpeechRecognition.m

--- src/ios/SpeechRecognition.m
+++ src/ios/SpeechRecognition.m
@@ -81,10 +81,11 @@
         }
 
         AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+        //[audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
         [audioSession setMode:AVAudioSessionModeMeasurement error:nil];
         [audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
-
+      
         self.recognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
         self.recognitionRequest.shouldReportPartialResults = showPartial;
 
